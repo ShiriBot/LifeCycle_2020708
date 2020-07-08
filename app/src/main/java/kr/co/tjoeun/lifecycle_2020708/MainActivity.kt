11cf25ddf30ac2b6088dfa12e7c4c18293400980
채정실 <cowjdtlf@gmail.com>
2020-07-08 22:21:04 +0900

    다른 화면으로 가는 Intent추가

--- app/src/main/java/kr/co/tjoeun/lifecycle_2020708/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_2020708/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.tjoeun.lifecycle_2020708
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("메인화면","OnCreate실행")
+
+        goToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
